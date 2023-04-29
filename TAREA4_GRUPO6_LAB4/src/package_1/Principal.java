bca605b0adb65963e490eee8872e5d018d2edd3c
Ivan Santiago <santylinares70@gmail.com>
2023-04-29 15:59:36 -0300

    commit

--- TAREA4_GRUPO6_LAB4/src/package_1/Principal.java
+++ TAREA4_GRUPO6_LAB4/src/package_1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Window ventana = new Window();
-		ventana.ChangeVisivility(true);
+		ventana.ChangeVisibility(true);
 	}
 
 }
--- TAREA4_GRUPO6_LAB4/src/package_1/Window.java
+++ TAREA4_GRUPO6_LAB4/src/package_1/Window.java
@@ -9,10 +9,8 @@ import java.awt.event.ActionEvent;
 
 public class Window extends JFrame 
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 	private JButton botonEjercicio1;
 	private JButton botonEjercicio2;
 	private JButton botonEjercicio3;
@@ -23,42 +21,35 @@ public class Window extends JFrame
 		setSize(294, 287);
 		setLocation(350, 350);
 		getContentPane().setLayout(null);
+		
 		AgregarBotones();
 	}
-	
-	public Window(int width, int high, int x, int y)
-	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(width, high);
-		setLocation(x, y);
-	}
-	
-	public void ChangeVisivility(boolean estado)
+		
+	public void ChangeVisibility(boolean estado)
 	{
 		setVisible(estado);
 	}
 	
-	public void AgregarBotones() {
+	public void AgregarBotones() 
+	{
 		botonEjercicio1 = new JButton();
-		botonEjercicio1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				WindowEj1 ej1 = new WindowEj1();
-			}
-		});
 		botonEjercicio1.setText("Ejercicio 1");
 		botonEjercicio1.setBounds(68, 70, 150, 30);
-		
-	    getContentPane().add(botonEjercicio1);
+		botonEjercicio1.addActionListener(new EventoBoton1());
+			
+		getContentPane().add(botonEjercicio1);
 	    
 		botonEjercicio2 = new JButton();
 		botonEjercicio2.setText("Ejercicio 2");
 		botonEjercicio2.setBounds(68, 130, 150, 30);
+		botonEjercicio1.addActionListener(new EventoBoton2());
 		
 	    getContentPane().add(botonEjercicio2);
 	    
 		botonEjercicio3 = new JButton();
 		botonEjercicio3.setText("Ejercicio 3");
 		botonEjercicio3.setBounds(68, 190, 150, 30);
+		botonEjercicio1.addActionListener(new EventoBoton3());
 		
 	    getContentPane().add(botonEjercicio3);
 	    
@@ -66,5 +57,38 @@ public class Window extends JFrame
 	    lblGrupo.setFont(new Font("Tahoma", Font.PLAIN, 16));
 	    lblGrupo.setBounds(22, 11, 126, 28);
 	    getContentPane().add(lblGrupo);
+	}
+	
+	class EventoBoton1 implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			WindowEj1 ej1 = new WindowEj1();
+		}
+		
+	}
+	
+	class EventoBoton2 implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			WindowEj1 ej2 = new WindowEj1(); //TODO: Crear la clase WindowsEj2 y reemplazar la que esta puesta aca
+		}
+		
+	}
+	
+	class EventoBoton3 implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			WindowEj1 ej3 = new WindowEj1(); //TODO: Crear la clase WindowsEj3 y reemplazar la que esta puesta aca
+		}
+		
 	}
 }
