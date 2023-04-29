915a5a0492f45b772c9338d56738bc2ecef0f4ad
GOTTIG, Marianella <mgottig-ext@aa2000.com.ar>
2023-04-29 00:20:59 -0300

    agrego cambios

--- TAREA4_GRUPO6_LAB4/src/package_1/Principal.java
+++ TAREA4_GRUPO6_LAB4/src/package_1/Principal.java
@@ -1,11 +1,11 @@
 package package_1;
+import javax.swing.JButton;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		Window ventana = new Window();
-		ventana.ChangeVisivility(true);
-
+		ventana.ChangeVisivility(true);	
 	}
 
 }
--- TAREA4_GRUPO6_LAB4/src/package_1/Window.java
+++ TAREA4_GRUPO6_LAB4/src/package_1/Window.java
@@ -1,14 +1,25 @@
 package package_1;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Window extends JFrame 
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JButton botonEjercicio1;
+	private JButton botonEjercicio2;
+	private JButton botonEjercicio3;
+	
 	public Window()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 500);
 		setLocation(350, 350);
+		setLayout(null);
+		AgregarBotones();
 	}
 	
 	public Window(int width, int high, int x, int y)
@@ -21,6 +32,26 @@ public class Window extends JFrame
 	public void ChangeVisivility(boolean estado)
 	{
 		setVisible(estado);
+	}
+	
+	public void AgregarBotones() {
+		botonEjercicio1 = new JButton();
+		botonEjercicio1.setText("Ejercicio 1");
+		botonEjercicio1.setBounds(175, 20, 150, 30);
+		
+	    getContentPane().add(botonEjercicio1);
+	    
+		botonEjercicio2 = new JButton();
+		botonEjercicio2.setText("Ejercicio 2");
+		botonEjercicio2.setBounds(175, 70, 150, 30);
+		
+	    getContentPane().add(botonEjercicio2);
+	    
+		botonEjercicio3 = new JButton();
+		botonEjercicio3.setText("Ejercicio 3");
+		botonEjercicio3.setBounds(175, 120, 150, 30);
+		
+	    getContentPane().add(botonEjercicio3);
 	}
 	
 }
